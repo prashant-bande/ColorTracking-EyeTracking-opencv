58db19ce334d45c33354405448401e52567fb26a
prashant-bande <45324633+prashant-bande@users.noreply.github.com>
2020-01-13 12:45:13 +0530

    initial commit

---
+++ colortracker.py
@@ -0,0 +1,37 @@
+import numpy as np
+import cv2
+import argparse
+import time
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", help = "path to the (optional) video file" )
+args = vars(ap.parse_args())
+
+blueLower = np.array([100, 67, 0], dtype = "uint8")
+blueUpper = np.array([255, 128, 50], dtype = "uint8")
+
+camera = cv2.VideoCapture(args["video"])
+
+while True:
+    (ok, frame) = camera.read()
+    if not ok:
+        break
+    blue = cv2.inRange(frame, blueLower, blueUpper)
+    blue = cv2.GaussianBlur(blue, (3, 3), 0)
+    
+    (cnts, _) = cv2.findContours(blue.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    cv2.drawContours(frame, cnts, -1, (0, 255, 0), 2) 
+        
+    cv2.imshow("Tracking color blue", frame)
+    cv2.imshow("Binary", blue)
+    
+    time.sleep(0.255)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+    
+camera.realease()
+cv2.destroyAllWindows()
+    
+    
+    
